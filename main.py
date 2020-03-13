ec5fc4a8e7acbe2aa0255ed6aab1f68b44062159
John Pham <johnphammail@gmail.com>
2020-03-12 22:11:38 -0700

    Add downloading photos from an album

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from os.path import join, dirname
+from os.path import join, dirname, exists
+from os import makedirs
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import AuthorizedSession
 from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 import json
+from urllib.request import urlretrieve as download
 
 AUTH_TOKEN_FILE = './secrets/auth_token_file.json'
 CLIENT_ID = './client_id.json'
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
           'https://www.googleapis.com/auth/photoslibrary.sharing']
 GOOGLE_PHOTOS_API = 'https://photoslibrary.googleapis.com/v1/albums'
+ALBUM_ID = 'AIg7YU3hCbEq6aDSrMkL63fwHhfot1RvHTfAa6nAnFTH4iwjfuSIvs24Qrlpc0QsRNxLgGeBFEvO62JYvgD1Gqtal19AE2pREA'
+PHOTOS_SAVE_DIRECTORY = 'photos'
 
 
 # Saves the auth token after going through auth flow
@@ -27,14 +32,28 @@ def save_credentials(cred, auth_file):
         print(json.dumps(cred_dict), file=f)
 
 
+def write_photos_to_disk(mediaItems):
+    for media in mediaItems:
+        if 'image' in media['mimeType']:
+            download_url = f"{media['baseUrl']}=d"
+
+            if not exists(PHOTOS_SAVE_DIRECTORY):
+                makedirs(PHOTOS_SAVE_DIRECTORY)
+
+            download(download_url,
+                     f"{PHOTOS_SAVE_DIRECTORY}/{media['filename']}")
+
+
 credentials = None
-session = None
 
 try:
     credentials = Credentials.from_authorized_user_file(
         AUTH_TOKEN_FILE, SCOPES)
-except OSError as err:
-    print("Error opening auth token file - {0}".format(err))
+except:
+    # This fails when we
+    # 1. Don't have a auth token file, that's fine, we'll generate one by authing with a client ID
+    # 2. The auth token file we have is malformed, again we'll generate a new one
+    pass
 
 if not credentials:
     auth_flow = InstalledAppFlow.from_client_secrets_file(
@@ -49,8 +68,8 @@ if not credentials:
 
     save_credentials(credentials, AUTH_TOKEN_FILE)
 
-session = AuthorizedSession(credentials)
-albums = session.get(
-    GOOGLE_PHOTOS_API).json()
+google_photos_service = build('photoslibrary', 'v1', credentials=credentials)
 
-print(albums)
+results = google_photos_service.mediaItems().search(
+    body={'albumId': ALBUM_ID}).execute()
+write_photos_to_disk(results['mediaItems'])
